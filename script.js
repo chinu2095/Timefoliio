f7fde7719aeba9e673b0564949e4ac4705e07700
chinu2095 <priyanshusharma2095@gmail.com>
2025-09-22 00:11:35 +0530

    updated the notification div

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const gallery = document.getElementById("gallery");
 
-window.onmousemove = e => {
+window.onmousemove = (e) => {
   const mouseX = e.clientX,
         mouseY = e.clientY;
   
   const xDecimal = mouseX / window.innerWidth,
         yDecimal = mouseY / window.innerHeight;
   
-  const maxX = gallery.offsetWidth - window.innerWidth,
-        maxY = gallery.offsetHeight - window.innerHeight;
+  const maxX = gallery.offsetWidth - window.innerWidth +400,
+        maxY = gallery.offsetHeight - window.innerHeight+400 ;
   
   const panX = maxX * xDecimal * -1,
         panY = maxY * yDecimal * -1;
@@ -21,3 +21,18 @@ window.onmousemove = e => {
     easing: "ease"
   })
 }
+
+document.addEventListener("DOMContentLoaded", ()=>{
+
+      const button = document.querySelector(".close-button");
+
+      if(button){
+            button.addEventListener("click",()=>{
+                  console.log("clicked");
+                  button.closest(".notification").style.display = "none";
+            }
+            )
+      } else{
+            console.log("something went wrong");
+      }
+});
